35065c1ab69ed56889f465f226637567019a6130
u112958 <gabriel_lopez-candales@bio-rad.com>
2023-12-16 15:19:58 -0500

    Modifications and Addtions - Updated the Cartridge and ExperimentData to use ComboBoxCells - Added a Configuration class for handling instrument, datapath, user default, etc from the added Configurations.XML data file in Resources - Updated BerquistOptions to return DataGridViewComboBoxCells - Removed some code regarding PartitionType and Cartridges from the DataGridViewFormatter utility to slowly make this class obsolete

---
+++ Models/Configuration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Independent_Reader_GUI.Models
+{
+    internal class Configuration
+    {
+        // FIXME: Replace this with a variable taken from a config file
+        public string ConfigurationDataPath = "C:\\Users\\u112958\\source\\repos\\Independent-Reader-GUI\\Resources\\Configuration\\Configuration.xml";
+        public string InstrumentName = string.Empty;
+        public string InstrumentID = string.Empty;
+        public string ThermocyclingProtocolsDataPath = string.Empty;
+        public string BergquistDataPath = string.Empty;
+        public string CartridgeDataPath = string.Empty;
+        public string DefaultPartitionType = string.Empty;
+        public string DefaultCartridge = string.Empty;
+
+        public Configuration()
+        {
+            ReadInConfigurationData();
+        }
+
+        private void ReadInConfigurationData()
+        {
+            // Load in the protocol file
+            XDocument xDocument = XDocument.Load(ConfigurationDataPath);
+            // Read in the config
+            var node = xDocument.Root;
+            InstrumentName = node.Element("InstrumentName").Value;
+            InstrumentID = node.Element("InstrumentID").Value;
+            ThermocyclingProtocolsDataPath = node.Element("ThermocyclingProtocolsDataPath").Value;
+            BergquistDataPath = node.Element("BergquistDataPath").Value;
+            CartridgeDataPath = node.Element("CartridgeDataPath").Value;
+            DefaultPartitionType = node.Element("DefaultPartitionType").Value;
+            DefaultCartridge = node.Element("DefaultCartridge").Value;
+        }
+    }
+}
---
+++ Resources/CartridgeOptions.cs
@@ -0,0 +1,58 @@
+﻿using Independent_Reader_GUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Independent_Reader_GUI.Resources
+{
+    internal class CartridgeOptions
+    {
+        // FIXME: Replace this with a variable taken from a config file
+        private string cartridgeDataPath = "C:\\Users\\u112958\\source\\repos\\Independent-Reader-GUI\\Resources\\Cartridges\\CartridgeData.xml";
+        private List<Cartridge> options = new List<Cartridge>();
+
+        public CartridgeOptions()
+        {
+            ReadInCartridgeData();
+        }
+
+        private void ReadInCartridgeData()
+        {
+            // Load in the protocol file
+            XDocument xDocument = XDocument.Load(cartridgeDataPath);
+            // Read in the Cartridge options
+            foreach (var cartridgeNode in xDocument.Descendants("Cartridge"))
+            {
+                Cartridge cartridge = new Cartridge();
+                cartridge.Name = cartridgeNode.Element("Name").Value;
+                cartridge.PartitionType = cartridgeNode.Element("PartitionType").Value;
+                cartridge.NumberofSampleChambers = int.Parse(cartridgeNode.Element("NumberofSampleChambers").Value.ToString());
+                cartridge.NumberofAssayChambers = int.Parse(cartridgeNode.Element("NumberofAssayChambers").Value.ToString());
+                cartridge.Length = double.Parse(cartridgeNode.Element("Length").Value.ToString());
+                cartridge.LengthUnits = cartridgeNode.Element("LengthUnits").Value;
+                cartridge.Width = double.Parse(cartridgeNode.Element("Width").Value.ToString());
+                cartridge.WidthUnits = cartridgeNode.Element("WidthUnits").Value;
+                cartridge.Height = double.Parse(cartridgeNode.Element("Height").Value.ToString());
+                cartridge.HeightUnits = cartridgeNode.Element("HeightUnits").Value;
+                options.Add(cartridge);
+            }
+        }
+
+        public DataGridViewComboBoxCell GetOptionNamesComboBoxCell(string partitionType)
+        {
+            DataGridViewComboBoxCell comboBoxCell = new DataGridViewComboBoxCell();
+            foreach (var option in options)
+            {
+                if (option.PartitionType.Equals(partitionType))
+                {
+                    comboBoxCell.Items.Add(option.Name);
+                }
+            }
+            comboBoxCell.Value = comboBoxCell.Items[0];
+            return comboBoxCell;
+        }
+    }
+}
