36ebbf0c6e868663d1d8e778e503ed0e7da1316e
u112958 <gabriel_lopez-candales@bio-rad.com>
2024-01-20 15:03:31 -0500

    Added functionality to start adding actions with the APE - Started working on Add Action button functionality for the APE, this includes the Form which appears. - Changed how versioning works on the Assay Protocols - Rearranged some files

---
+++ Forms/AssayProtocolAddActionForm.cs
@@ -0,0 +1,34 @@
+﻿using Independent_Reader_GUI.Models;
+using Independent_Reader_GUI.Models.Consumables;
+using Independent_Reader_GUI.Models.Protocols.AssayProtocol;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Independent_Reader_GUI.Forms
+{
+    public partial class AssayProtocolAddActionForm : Form
+    {
+        public AssayProtocolAddActionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SetupTipsTabPage()
+        {
+            //
+            // Setup ComboBoxes and their Default values
+            //
+            addTipsTypeComboBox.DataSource = AssayProtocolActionTips.TypeOptions;
+            addTipsTypeComboBox.Text = AssayProtocolActionTips.TypeOptions[0];
+            addTipsTipComboBox.DataSource = TipOptions.Options;
+            addTipsTipComboBox.Text = TipOptions.Options[0];
+        }
+    }
+}
